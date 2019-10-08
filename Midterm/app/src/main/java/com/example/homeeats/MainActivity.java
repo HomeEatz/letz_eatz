b06bdda7920146bb7a62411cb08a9c317e721673
mrgallegos28 <56003622+mrgallegos28@users.noreply.github.com>
2019-10-08 17:26:07 -0400

    Add files via upload

---
+++ Midterm/app/src/main/java/com/example/homeeats/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.homeeats;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_eats_main_screen);
+    }
+}
