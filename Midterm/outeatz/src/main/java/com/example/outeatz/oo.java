03c6a483d08165c7b5191bf0d308d4cdf48d3787
Meredith Gallegos <MRGallegos@student.fullsail.edu>
2019-10-08 18:37:17 -0400

    official starting app

---
+++ Midterm/outeatz/src/main/java/com/example/outeatz/oo.java
@@ -0,0 +1,14 @@
+package com.example.outeatz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class oo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oo);
+    }
+}
